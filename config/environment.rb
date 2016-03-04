9707d73625bff0fb62b8e95f0265b2e45731daf4
Mikias Abera <itsmikias@gmail.com>
2016-03-04 17:35:50 +0300

    Envrionment.rb edit

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.raise_delivery_errors = true
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
