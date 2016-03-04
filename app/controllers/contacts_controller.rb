f16e05be2815738ae6829adca9fea95da53dfdbd
Mikias Abera <itsmikias@gmail.com>
2016-03-04 11:33:54 +0300

    Fixed contact.rb, created contacts form view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,8 @@
-class ContactController < ApplicationController
-	def new
-	end
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
 
-	def create
-	end
+  def create
+  end
 end
