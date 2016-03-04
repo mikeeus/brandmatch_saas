dd40e5fadc0c34ca69d41299b08b2c73b846c179
Mikias Abera <itsmikias@gmail.com>
2016-03-04 17:02:59 +0300

    Add sendgrid credentials

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV['app48185283@heroku.com'],
+  :password => ENV['wc1v6ydy4208'],
   :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
