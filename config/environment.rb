2424665ec746cfca87401825456b688d4446febc
Mikias Abera <itsmikias@gmail.com>
2016-03-04 18:03:55 +0300

    Remove envrionment mail settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :enable_startstls_auto => true
-}
