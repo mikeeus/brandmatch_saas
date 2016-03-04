f92dcd57166439f02d6e874b06d72dff53717cbf
Mikias Abera <itsmikias@gmail.com>
2016-03-04 17:18:33 +0300

    Added delivery method and raise delivery errors in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,15 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+config.action_mailer.raise_delivery_errors = true
+
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['app48185283@heroku.com'],
-  :password => ENV['wc1v6ydy4208'],
+  :user_name => ENV['wc1v6ydy4208'],
+  :password => ENV['app48185283@heroku.com'],
   :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
