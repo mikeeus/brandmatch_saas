8db3ea1659dd1d9f0ffa9cd5ec8660ff870b9157
Mikias Abera <itsmikias@gmail.com>
2016-03-04 16:32:19 +0300

    Add actionmailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp_settings.net'
+  :port => 587,
+  :authentication => :plain,
+  :user_name => ENV|'SENDGRID_USERNAME'|,
+  :password => 'heroku.com',
+  :enable_startstls_auto => true
+}
