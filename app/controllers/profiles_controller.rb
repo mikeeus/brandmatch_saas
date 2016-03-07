ba7c60d1f79e2ac61250c57c01a3a533a5693a85
Mikias Abera <itsmikias@gmail.com>
2016-03-07 18:00:51 +0300

    Add profile new action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,7 @@
 class ProfilesController < ApplicationController
-
+  def new
+    # Form for a user to fill out their own profile
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end
 end
