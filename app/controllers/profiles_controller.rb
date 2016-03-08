64d15ea5b820bf70aebde9fafa72d519a542bb2b
Mikias Abera <itsmikias@gmail.com>
2016-03-08 10:54:13 +0300

    Change new action in profiles, and edit type in user model

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def new
     # Form for a user to fill out their own profile
     @user = User.find( params[:user_id] )
-    @profile = @user.build_profile
+    @profile = Profile.new
   end
   
   def create
