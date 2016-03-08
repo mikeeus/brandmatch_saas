0365eb64f3ddfe6425109a82872c1279c6ede236
Mikias Abera <itsmikias@gmail.com>
2016-03-08 15:19:24 +0300

    Improve user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    
+    @users = User.all
   end
 
   def show
     @user = User.find( params[:id] )
+    # what if user requests a path "users/user_id" user id that doesn't exist?
   end
 end
