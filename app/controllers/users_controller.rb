4ea03f1486425676d1c842f8d30528db1440a3fc
Mikias Abera <itsmikias@gmail.com>
2016-03-08 11:55:43 +0300

    Fix type in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authentication_user!
-  
+  before_action :authenticate_user!
+
   def show
     @user = User.find( params[:id] )
   end
